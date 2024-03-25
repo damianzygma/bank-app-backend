67312a331a9fe732a3c8cf57e985d40d3f8c9ad8
damianzygma <damian.zygma@gmail.com>
2024-03-25 16:11:56 +0100

    chore: Add registration of new User with Bcrypt password encoder

--- src/main/java/com/damianzygma/bankappbackend/controller/LoginController.java
+++ src/main/java/com/damianzygma/bankappbackend/controller/LoginController.java
@@ -5,6 +5,7 @@ import com.damianzygma.bankappbackend.repository.CustomerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,11 +16,17 @@ public class LoginController {
     @Autowired
     CustomerRepository customerRepository;
 
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+
     @PostMapping("/register")
     public ResponseEntity<String> registerUser(@RequestBody Customer customer){
         Customer savedCustomer = null;
         ResponseEntity response = null;
         try{
+            String hashPassword = passwordEncoder.encode(customer.getPassword());
+            customer.setPassword(hashPassword);
             savedCustomer = customerRepository.save(customer);
             if(savedCustomer.getId() > 0){
                 response = ResponseEntity
