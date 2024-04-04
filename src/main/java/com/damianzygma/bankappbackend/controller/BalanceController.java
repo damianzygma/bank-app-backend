df0d94e6ee089d0e51659ada8390bb06548e0bbf
damianzygma <damian.zygma@gmail.com>
2024-04-04 16:11:02 +0200

    chore: Update BalanceController

--- src/main/java/com/damianzygma/bankappbackend/controller/BalanceController.java
+++ src/main/java/com/damianzygma/bankappbackend/controller/BalanceController.java
@@ -1,13 +1,27 @@
 package com.damianzygma.bankappbackend.controller;
 
+import com.damianzygma.bankappbackend.model.AccountTransactions;
+import com.damianzygma.bankappbackend.repository.AccountTransactionsRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class BalanceController {
 
+    @Autowired
+    private AccountTransactionsRepository accountTransactionsRepository;
+
     @GetMapping("/myBalance")
-    public String getBalanceDetails(){
-        return "Here are the balance details form the database";
+    public List<AccountTransactions> getBalanceDetails(@RequestParam int id){
+        List<AccountTransactions> accountTransactions = accountTransactionsRepository.findByCustomerIdOrderByTransactionDtDesc(id);
+        if (accountTransactions != null) {
+            return accountTransactions;
+        } else {
+            return null;
+        }
     }
 }
