45dc67ee26fc7d4560385fdf5b3a372269070dec
damianzygma <damian.zygma@gmail.com>
2024-04-04 16:37:34 +0200

    chore: Update LoginController

--- src/main/java/com/damianzygma/bankappbackend/controller/LoginController.java
+++ src/main/java/com/damianzygma/bankappbackend/controller/LoginController.java
@@ -5,16 +5,20 @@ import com.damianzygma.bankappbackend.repository.CustomerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class LoginController {
 
     @Autowired
-    CustomerRepository customerRepository;
+    private CustomerRepository customerRepository;
 
     @Autowired
     private PasswordEncoder passwordEncoder;
@@ -41,6 +45,14 @@ public class LoginController {
         return response;
     }
 
-
-
+    @RequestMapping("/user")
+    public Customer getUserDetailsAfterLogin(Authentication authentication){
+        List<Customer> customers = customerRepository.findByEmail(authentication.getName());
+        if (customers.size() > 0) {
+            return customers.get(0);
+        } else {
+            return null;
+        }
+    }
+    
 }
