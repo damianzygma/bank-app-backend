f85b7be64586b160276abeb336a1eb04e9b021ff
damianzygma <damian.zygma@gmail.com>
2024-04-04 16:23:03 +0200

    chore: Update Loans Controller

--- src/main/java/com/damianzygma/bankappbackend/controller/LoansController.java
+++ src/main/java/com/damianzygma/bankappbackend/controller/LoansController.java
@@ -1,14 +1,28 @@
 package com.damianzygma.bankappbackend.controller;
 
+import com.damianzygma.bankappbackend.model.Loans;
+import com.damianzygma.bankappbackend.repository.LoanRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class LoansController {
 
+    @Autowired
+    private LoanRepository loanRepository;
+
     @GetMapping("/myLoans")
-    public String getLoanDetails(){
-        return "Here are the loan details from the database";
+    public List<Loans> getLoanDetails(@RequestParam int id){
+        List<Loans> loans = loanRepository.findByCustomerIdOrderByStartDtDesc(id);
+        if (loans != null){
+            return loans;
+        } else {
+            return null;
+        }
     }
 
 }
