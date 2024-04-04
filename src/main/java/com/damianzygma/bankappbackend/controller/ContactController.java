b07cd79595e68043bf1f69ee959c564275906cb0
damianzygma <damian.zygma@gmail.com>
2024-04-04 16:20:24 +0200

    chore: Update ContactController

--- src/main/java/com/damianzygma/bankappbackend/controller/ContactController.java
+++ src/main/java/com/damianzygma/bankappbackend/controller/ContactController.java
@@ -1,14 +1,33 @@
 package com.damianzygma.bankappbackend.controller;
 
+import com.damianzygma.bankappbackend.model.Contact;
+import com.damianzygma.bankappbackend.repository.ContactRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Date;
+import java.util.Random;
 
 @RestController
 public class ContactController {
 
-    @GetMapping("/contact")
-    public String saveContactInquiryDetails(){
-        return "Inquiry details are saved to the database";
+    @Autowired
+    private ContactRepository contactRepository;
+
+    @PostMapping("/contact")
+    public Contact saveContactInquiryDetails(@RequestBody Contact contact){
+        contact.setContactId(getServiceReqNumber());
+        contact.setCreateDt(new Date(System.currentTimeMillis()));
+        return contactRepository.save(contact);
+    }
+
+    public String getServiceReqNumber(){
+        Random random = new Random();
+        int ranNum = random.nextInt(999999999 - 9999) + 9999;
+        return "SR"+ranNum;
     }
 
 }
